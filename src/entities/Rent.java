2f385f4be29bcc7adbbb532b04d7fcc36842d525
David Almeida <davidalmeidadrive@gmail.com>
2020-02-04 12:19:13 -0300

    class Rent

---
+++ src/entities/Rent.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class Rent {
+	
+	private String name;
+	private String email;
+	
+	public Rent(String name, String email) {
+		this.name = name;
+		this.email = email;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public String toString() {
+		return name + ", " + email;
+	}
+
+}
